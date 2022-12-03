14a178b8d3054d0f23700b7c04e77cd560796ef8
SebastianS13 <50264212+SebastianS13@users.noreply.github.com>
2022-12-03 00:25:46 +0000

    Added more settings

--- Password Generator/Src/Main.cpp
+++ Password Generator/Src/Main.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 
-const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int string_length = sizeof(alphanum) - 1;
+const char FullPossibility[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int FullPossibility_length = sizeof(FullPossibility) - 1;
+
+const char NoSymbols[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int NoSymbols_length = sizeof(NoSymbols) - 1;
+
+bool Symbols;
+int Length;
+
+std::string Generate()
+{
+    std::string Password;
+
+    if (Symbols == false)
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            std::cout << NoSymbols[rand() % NoSymbols_length];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            std::cout << FullPossibility[rand() % FullPossibility_length];
+        }
+    }
+
+    return Password;
+}
 
 int main()
 {
     srand(time(0));
-    int Length;
 
     std::cout << "Enter the length of password: ";
     std::cin >> Length;
-    std:: cout << "Generated password: ";
+    std::cout << "Should it contain symbols (y/n): ";
+    std::cin >> Symbols;
+    std::cout << "Generated password: ";
     
-    for (int i = 0; i < Length; i++)
-    {
-        std::cout << alphanum[rand() % string_length];
-    }
-    
+    std::cout << Generate();
+
+    std::cout << std::endl;
     std::cout << std::endl;
     std::cout << std::endl;
 
@@ -24,6 +51,9 @@ int main()
     system("pause >nul");
     system("cls");
     main();
+
+    Length = NULL;
+    Symbols = NULL;
 
     return 0;
 }
